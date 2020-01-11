c50994e43f523914cc02eb25e141e3d2eb6ff521
Ankur Anant <ankuranant99@gmail.com>
2020-01-11 13:23:11 +0530

    power webhook

---
+++ routes/powerRoute.js
@@ -0,0 +1,28 @@
+const route = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const PowerService = require('../Services/Power.service');
+
+const { verifyToken } = require('../utils/verifyToken');
+
+
+route.get('/', async (req, res) => {
+	// jwt.verify(req.token, process.env.JWT_SECRET, async (err, response) => {
+	// 	if(err) throw err;
+	// 	if(response) {
+			const powerData = await PowerService.getPower();
+			const latestPowerData = powerData[powerData.length - 1];
+			console.log('tapped ------>>' + latestPowerData);
+			res.status(200).json({power: latestPowerData.power, addedOn: latestPowerData.addedOn, isLoggedIn: true});
+		// } else {
+			// res.status(403).json({isLoggedIn: false});
+		// }
+	// });
+});
+
+route.post('/', async (req, res) => {
+	const newPowerData = await PowerService.createPower(req.body.power);
+	console.log("Power --------> ", req.body.power);
+})
+
+module.exports = route;
